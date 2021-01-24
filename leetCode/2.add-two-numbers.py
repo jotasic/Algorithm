b9cea285165dbf33de57e3fafd5273317f0270cb
jotasic <embedded61@gmail.com>
2021-01-24 10:53:20 +0900

    add file

---
+++ leetCode/2.add-two-numbers.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        class ListNode result
+        
+
+
+        digit = 0
+        totalValue = 0
+        nextAddValue = 0
+        while l1 != None :
+            sumValue = l1.val + l2.val 
+
+            if sumValue > 10 :
+                nextAddValue = 1
+                sumValue = sumValue - 10
+            else
+                nextAddValue = 0
+            
+            
+
+            
---
+++ leetCode/3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        existStr = set()
+        longestStrLen = 0
+        startIdx = 0
+        endIdx = 0
+        
+
+        for i in range(len(s)) :
+            
+            existStr.add()
+
+
+
+
+
+        
+
+
+
+
+
+
+
+s = Solution()
+print(s.lengthOfLongestSubstring("dvdf"))
